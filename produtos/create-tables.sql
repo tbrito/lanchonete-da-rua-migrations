47b9291bd951287abe8cc7f55498a57aef21ecdf
Tiago Brito <e3205@itsector.pt>
2023-12-10 19:54:59 -0300

    feat: initial database tables

---
+++ produtos/create-tables.sql
@@ -0,0 +1,17 @@
+--liquibase formatted sql
+
+--changeset your.name:1 labels:example-label context:example-context
+--comment: example comment
+CREATE TABLE public.categoria (
+	id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
+	nome varchar NULL,
+	created_at timestamp NULL
+);
+
+CREATE TABLE public.produto (
+	id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
+	nome varchar NULL,
+	categoria_id varchar NULL,
+	descricao varchar NULL,
+	created_at timestamp NULL
+);
