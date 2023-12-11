eef91974c0f959fa238e8ca793fc2e2bfe15cc3a
Tiago Brito <e3205@itsector.pt>
2023-12-11 08:17:54 -0300

    feat: insert products sql

--- produtos/202312110756_insertProducts.sql
+++ produtos/202312110756_insertProducts.sql
@@ -4,7 +4,6 @@ insert into categoria (nome, created_at) values ('Bebida', current_timestamp)
 insert into categoria (nome, created_at) values ('Sobremesa', current_timestamp)
 insert into categoria (nome, created_at) values ('Combo', current_timestamp)
 
-
 insert into produto (nome, descricao, categoria_id, valor, created_at) values ('Hamburger Brutal', 'O Queridinho Da Galera! Nosso burger mais pedido vai com 180g do nosso delicioso blend angus, pão australiano, maionese especial, bacon defumado, e queijo cheddar... (O duplo vai com o dobro de carne e queijo)', 1, 35.6, current_timestamp)
 insert into produto (nome, descricao, categoria_id, valor, created_at) values ('Hamburger Especial', 'Seu novo burger favorito! Nosso novo burger vai com nosso blend 180g angus, pão pretzel bem macio, queijo minas que além de um sabor delicioso, traz uma textura única para seu burger. E pra finalizar: um catchup de goiabada feito na casa! Um clássico da culinária agora no seu burger!', 1, 45.6, current_timestamp)
 insert into produto (nome, descricao, categoria_id, valor, created_at) values ('Hamburger Medieval', 'Com Catupiry De Verdade! Nosso mais novo burger vai com nosso blend 180g angus, pão pretzel, bacon defumado, , queijo cheddar e o legítimo Catupiry que dá uma cremosidade inigualável! (O duplo vai com o dobro de carne e queijo)', 1, 30.6, current_timestamp)
