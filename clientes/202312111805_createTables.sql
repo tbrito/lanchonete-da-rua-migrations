39ac84ef02900113caefd767ac87da06f4799152
Tiago Brito <e3205@itsector.pt>
2024-01-29 14:23:25 -0300

    feat: client table

--- clientes/202312111805_createTables.sql
+++ clientes/202312111805_createTables.sql
@@ -3,9 +3,10 @@
 --changeset your.name:1 labels:example-label context:example-context
 --comment: example comment
 CREATE TABLE public.cliente (
-	id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
-	nome varchar NULL,
-	cpf varchar NULL,
-	telefone varchar NULL,
-	created_at timestamp NULL
+    Id UUID PRIMARY KEY,
+    Nome TEXT NOT NULL,
+    DataNascimento TIMESTAMP NOT NULL,
+    Telefone TEXT NOT NULL,
+    Endereco TEXT NOT NULL,
+    Email TEXT NOT NULL
 );
